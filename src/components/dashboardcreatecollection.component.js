5c31e5959cb4ce90ea157746985ccd35f51f5be5
Conor Boyland <boy17005757@btc.ac.uk>
2021-06-04 00:08:34 +0100

    Fixed date time picker view on mobile devices.

--- src/components/dashboardcreatecollection.component.js
+++ src/components/dashboardcreatecollection.component.js
@@ -22,54 +22,24 @@ import CollectionService from '../services/collection.service';
 export default class DashboardCreateCollection extends Component {
 
     //register constructor
-  constructor(props) {
+    constructor(props) {
 
-    //allow access to props within constructor
-    super(props);
+        //allow access to props within constructor
+        super(props);
 
-    //assign default state
-    this.state = {
-        // name: '',
-        // description: '',
-        // price: '',
-        // status: 'Active',
-        loading: false,
-        showErrorAlert: false,
-        message: '',
-        currentUser: AuthenticationService.getCurrentUser(),
-        redirect: null,
-        dateTimeFrom: new Date(),
-        dateTimeTo: new Date()
+        //assign default state
+        this.state = {
+            loading: false,
+            showErrorAlert: false,
+            message: '',
+            currentUser: AuthenticationService.getCurrentUser(),
+            redirect: null,
+            dateTimeFrom: new Date(),
+            dateTimeTo: new Date()
 
-    };
+        };
 
-  }
-
-  //invoked after component is mounted
-  componentDidMount() {
-
-    // if(!this.props.adminUser) {
-
-    //   this.setState({
-
-    //     redirect: '/'
-
-    //   });
-
-    // }
-
-  }
-
-  //handle form entry
-//   handleChange(event) {
-
-//     this.setState({
-
-//         [event.target.id]: event.target.value
-
-//     });
-
-//   }
+    }
 
     handleDateTimeFrom(value) {
 
@@ -89,67 +59,67 @@ export default class DashboardCreateCollection extends Component {
 
     }
 
-//form submit on register
-onSubmit(event) {
+    //form submit on register
+    onSubmit(event) {
 
-    //prevent browser refresh after submit
-    event.preventDefault();
+        //prevent browser refresh after submit
+        event.preventDefault();
 
-    var self = this;
-
-    self.setState({
-
-        message: '',
-        loading: true
-
-    });
-
-    CollectionService.createCollection(self.state.dateTimeFrom, self.state.dateTimeTo)
-    .then(function (collection) {
-
-        if (!collection) {
-
-            self.setState({
-
-                showErrorAlert: true,
-                message: collection.data.message
-
-            });
-
-        } else {
-
-            self.setState({
-
-                showErrorAlert: false,
-                redirect: '/dashboard/collections'
-
-            });
-
-        }
-
-    })
-    .catch(function (error) {
-
-        console.error(error);
+        var self = this;
 
         self.setState({
 
-            showErrorAlert: true
+            message: '',
+            loading: true
 
         });
 
-    })
-    .finally(function () {
+        CollectionService.createCollection(self.state.dateTimeFrom, self.state.dateTimeTo)
+            .then(function (collection) {
 
-        self.setState({
+                if (!collection) {
 
-            loading: false
+                    self.setState({
 
-        });
+                        showErrorAlert: true,
+                        message: collection.data.message
 
-    });
+                    });
 
-  }
+                } else {
+
+                    self.setState({
+
+                        showErrorAlert: false,
+                        redirect: '/dashboard/collections'
+
+                    });
+
+                }
+
+            })
+            .catch(function (error) {
+
+                console.error(error);
+
+                self.setState({
+
+                    showErrorAlert: true
+
+                });
+
+            })
+            .finally(function () {
+
+                self.setState({
+
+                    loading: false
+
+                });
+
+            });
+
+    }
 
   //render login component
   render() {
@@ -163,15 +133,15 @@ onSubmit(event) {
 
     return (
         <div className="flex items-center justify-center bg-gray-200 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="bg-white dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg py-16 px-20 w-full">
+            <div className="bg-white dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg py-16 px-10 w-full">
                 <div className="max-w-md w-full space-y-8 mx-auto">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create Collection</h2>
                 </div>
 
-                <form onSubmit={this.onSubmit.bind(this)} className="mt-8 space-y-6">
+                <form onSubmit={this.onSubmit.bind(this)} className="mt-8">
                     <input type="hidden" name="remember" defaultValue="true" />
-                    <div className="rounded-md shadow-sm -space-y-px">
+                    <div className="rounded-md shadow-sm">
 
                     <p className="leading-relaxed font-bold">Date & Time From</p>
                     <div>
@@ -182,7 +152,7 @@ onSubmit(event) {
                         autofocus
                         showLeadingZeros={true}
                         required
-                        className="z-40 appearance-none rounded-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm my-4"
+                        className="z-40 w-full my-4"
                     />
                     </div>
 
@@ -194,7 +164,7 @@ onSubmit(event) {
                         minDate={this.state.dateTimeFrom}
                         showLeadingZeros={true}
                         required
-                        className="z-40 appearance-none rounded-none block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm my-4"
+                        className="z-40 w-full my-4"
                     />
                     </div>
                     <div>
