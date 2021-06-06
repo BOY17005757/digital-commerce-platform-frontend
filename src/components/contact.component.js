fbce2d22a83bbe028a23520dfc8a7f4db0341e65
Conor Boyland <boy17005757@btc.ac.uk>
2021-06-06 20:07:30 +0100

    Amended static contact details.

--- src/components/contact.component.js
+++ src/components/contact.component.js
@@ -204,14 +204,14 @@ export default class Contact extends Component {
                                 <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                                 </svg>
-                                <span className="mt-2">121 Street, NY</span>
+                                <span className="mt-2 text-center">Unit 6, The Burghley Centre, Hereward St, Bourne, PE10 9EG</span>
                             </p>
 
                             <p className="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
                                 <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                                 </svg>
-                                <span className="mt-2">+2499999666600</span>
+                                <span className="mt-2">+441778 425880</span>
                             </p>
 
                             <p className="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
@@ -219,7 +219,7 @@ export default class Contact extends Component {
                                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                                 </svg>
-                                <span className="mt-2">example@example.com</span>
+                                <span className="mt-2">contact@dgcmp.com</span>
                             </p>
                         </div>
                         </div>
