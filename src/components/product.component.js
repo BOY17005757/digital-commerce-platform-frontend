5fdc4009e1d0f248e736718c2bf23e100bd20149
Conor Boyland <boy17005757@btc.ac.uk>
2021-06-07 14:50:54 +0100

    Amended add to cart button colour.

--- src/components/product.component.js
+++ src/components/product.component.js
@@ -184,7 +184,7 @@ export default class Product extends Component {
                     View
                     </Link>
                     {(this.state.currentUser !== null && (
-                        <button onClick={() => this.addToShoppingCart(value._id)} className="my-4 flex ml-2 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base text-white bg-indigo-600 hover:bg-indigo-700">
+                        <button onClick={() => this.addToShoppingCart(value._id)} className="my-4 flex ml-2 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-indigo-700 bg-indigo-200 hover:bg-indigo-300">
                             Add to Cart
                         </button>
                     ))}
--- src/components/productdetail.component.js
+++ src/components/productdetail.component.js
@@ -135,7 +135,7 @@ export default class ProductDetail extends Component {
                             <span className="title-font text-2xl text-gray-900">£{this.state.product.price}</span>
                         </div>
                         {(this.state.currentUser !== null && (
-                            <button onClick={this.onClick.bind(this)} className="my-4 flex ml-auto whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base text-white bg-indigo-600 hover:bg-indigo-700">
+                            <button onClick={this.onClick.bind(this)} className="my-4 flex whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-indigo-700 bg-indigo-200 hover:bg-indigo-300">
                                 Add to Cart
                             </button>
                         ))}
